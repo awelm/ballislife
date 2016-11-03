af8c11f57a320723ce9f9fa27f08f37c763237b1
lawrencey99 <lawrencey99@gmail.com>
2016-11-02 23:49:42 -0700

    init commit

---
+++ create_tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE boxscore(
+  game_id varchar(20) NOT NULL,
+  team_id varchar(20) NOT NULL,
+  starting_position varchar(5),
+  seconds_played integer,
+  fg_m integer,
+  fg_a integer,
+  fg_pct float8,
+  threes_m integer,
+  threes_a integer,
+  threes_pct float8,
+  ft_m integer,
+  ft_a integer,
+  ft_pct float8,
+  oreb integer,
+  dreb integer,
+  rebounds integer,
+  assists integer,
+  steals integer,
+  blocks integer,
+  turnovers integer,
+  personal_fouls integer,
+  points integer,
+  plus_minus integer
+);
+
+CREATE TABLE gamesummary(
+  game_id varchar (20),
+  game_date timestamp,
+  home_team_id varchar(20),
+  visitory_team_id varchar(20),
+  season varchar(10),
+  home_points integer,
+  away_points integer
+);
---
+++ db_connector.py
@@ -0,0 +1,6 @@
+import psycopg2
+
+class PSQL_Connector(object):
+  def __init__:
+    try:
+      conn = psycopg2.connect("dbname"='nba'
