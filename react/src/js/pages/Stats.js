3fe2f51a6871eece11cfb4f15a536f480aceb621
Amila Welihinda <amilajack@gmail.com>
2016-11-03 22:01:03 -0700

    Initial working scatterplot implementation

--- react/src/js/pages/Stats.js
+++ react/src/js/pages/Stats.js
@@ -1,46 +1,50 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import React, { Component } from 'react';
+import Chartist from 'chartist';
+// import Chartist from 'react-chartist';
 
-export default function Stats() {
-  const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'My First dataset',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40],
-        spanGaps: false
+export default class Stats extends Component {
+  componentDidMount() {
+    const times = n => Array(...new Array(n));
+    const data = times(52).map(Math.random).reduce((_data, rnd, index) => {
+      _data.labels.push(index + 1);
+      _data.series.forEach((series) => {
+        series.push(Math.random() * 100);
+      });
+
+      return _data;
+    }, {
+      labels: [],
+      series: times(4).map(() => [])
+    });
+
+    const options = {
+      showLine: false,
+      axisX: {
+        labelInterpolationFnc(value, index) {
+          return index % 13 === 0 ? `W${value}` : null;
+        }
       }
-    ]
-  };
+    };
 
-  return (
-    <div>
-      <h1>Stats</h1>
-      <Bar
-        data={data}
-        width={100}
-        height={50}
-        options={{
-          maintainAspectRatio: false
-        }}
-      />
-    </div>
-  );
+    const responsiveOptions = [
+      ['screen and (min-width: 640px)', {
+        axisX: {
+          labelInterpolationFnc(value, index) {
+            return index % 4 === 0 ? `W${value}` : null;
+          }
+        }
+      }]
+    ];
+
+    new Chartist.Line('.ct-chart', data, options, responsiveOptions);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Stats</h1>
+        <div className="ct-chart" />
+      </div>
+    );
+  }
 }
