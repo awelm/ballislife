c4c8c9e90293e1abeec2c590f33e5cf081915872
Bryan Tran <bryanvantran@ucla.edu>
2016-11-27 21:29:44 -0800

    stats page sort by season

--- react/src/js/actions/StatsActions.js
+++ react/src/js/actions/StatsActions.js
@@ -12,7 +12,24 @@ export function getAllLeaders() {
     })
     .catch((error) => {
       dispatcher.dispatch({type: "RECEIVE_ALL_LEADERS_ERROR"});
-      console.log('error received')
+      // console.log('error received')
       console.error(error);
     });
+}
+
+export function getSeasonLeaders(season) {
+  // dispatcher.dispatch({type: "FETCH_PLAYER_NEWS"});
+    var myRequest = new Request(`http://localhost:5000/allleaders?Season=` + season);
+    fetch(myRequest)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        dispatcher.dispatch({type: "RECEIVE_SEASON_LEADERS", leaders: responseJson });
+        // console.log(responseJson);
+        // return(responseJson);
+      })
+      .catch((error) => {
+        dispatcher.dispatch({type: "RECEIVE_SEASON_LEADERS_ERROR"});
+        console.log('error received')
+        console.error(error);
+      });
 }
--- react/src/js/pages/Stats.js
+++ react/src/js/pages/Stats.js
@@ -12,25 +12,44 @@ export default class Stats extends React.Component {
   constructor() {
     super();
     this.getLeaders = this.getLeaders.bind(this);
+    this.getSeasonLeaders = this.getSeasonLeaders.bind(this);
+    this.handleSeasonInput = this.handleSeasonInput.bind(this);
     this.state = {
       curLeaders: StatsStore.getCurLeaders(),
-      allTimeLeaders: StatsStore.getAllTimeLeaders()
+      season: "2015-16",
+      seasonInfo: StatsStore.getSeasonLeaders()
     }
   }
 
   componentWillMount() {
     StatsStore.on("change", this.getLeaders);
+    StatsStore.on("changeSeason", this.getSeasonLeaders);
     StatsActions.getAllLeaders();
+    StatsActions.getSeasonLeaders(this.state.season);
   }
 
   componentWillUnmount() {
     StatsStore.removeListener("change", this.getLeaders);
+    StatsStore.removeListener("changeSeason", this.getSeasonLeaders);
   }
 
   getLeaders() {
     this.setState({
       curLeaders: StatsStore.getCurLeaders(),
-      allTimeLeaders: StatsStore.getAllTimeLeaders()
+    });
+  }
+
+  getSeasonLeaders() {
+    // console.log('getting season leaders');
+    this.setState({
+      seasonInfo: StatsStore.getSeasonLeaders()
+    });
+  }
+
+  handleSeasonInput(season) {
+    // console.log('handling season input');
+    this.setState({
+      season: season
     });
   }
 
@@ -42,7 +61,7 @@ export default class Stats extends React.Component {
       "http://nutsandboltssports.com/wp-content/uploads/2016/10/westbrook.jpg"
     ].map((img_url, i) => <Image key={i} img_url={img_url} />);
 
-    const { curLeaders, allTimeLeaders } = this.state;
+    const { curLeaders, seasonInfo } = this.state;
 
     const curLeadersPts = curLeaders['pts'];
     const curLeadersReb = curLeaders['reb'];
@@ -66,21 +85,85 @@ export default class Stats extends React.Component {
     return (
       <div class="container">
         <h1>Current Leaders</h1>
-          <div class="row">
-            {Images}
-          </div>
-          <div class="row">
-            <LeaderList leaderList={curLeaders['pts']} stat='pts' />
-            <LeaderList leaderList={curLeaders['reb']} stat='reb' />
-            <LeaderList leaderList={curLeaders['ast']} stat='ast' />
-            <LeaderList leaderList={curLeaders['fgp']} stat='fgp' />
-            <LeaderList leaderList={curLeaders['ftp']} stat='ftp' />
-            <LeaderList leaderList={curLeaders['fg3p']} stat='fg3p' />
-            <LeaderList leaderList={curLeaders['stl']} stat='stl' />
-            <LeaderList leaderList={curLeaders['blk']} stat='blk' />
+        <div class="row">
+          {Images}
+        </div>
+        <div class="row">
+          <LeaderList leaderList={curLeaders['pts']} stat='pts' />
+          <LeaderList leaderList={curLeaders['reb']} stat='reb' />
+          <LeaderList leaderList={curLeaders['ast']} stat='ast' />
+          <LeaderList leaderList={curLeaders['fgp']} stat='fgp' />
+          <LeaderList leaderList={curLeaders['ftp']} stat='ftp' />
+          <LeaderList leaderList={curLeaders['fg3p']} stat='fg3p' />
+          <LeaderList leaderList={curLeaders['stl']} stat='stl' />
+          <LeaderList leaderList={curLeaders['blk']} stat='blk' />
+        </div>
 
-          </div>
+        <hr class="spacer" />
+        <h1>Past Leaders</h1>
+        <SearchBar onSeasonInput={this.handleSeasonInput} season={this.state.season} />
+        <div class="row">
+          <LeaderList leaderList={seasonInfo['pts']} stat='pts' />
+          <LeaderList leaderList={seasonInfo['reb']} stat='reb' />
+          <LeaderList leaderList={seasonInfo['ast']} stat='ast' />
+          <LeaderList leaderList={seasonInfo['fgp']} stat='fgp' />
+          <LeaderList leaderList={seasonInfo['ftp']} stat='ftp' />
+          <LeaderList leaderList={seasonInfo['fg3p']} stat='fg3p' />
+          <LeaderList leaderList={seasonInfo['stl']} stat='stl' />
+          <LeaderList leaderList={seasonInfo['blk']} stat='blk' />
+        </div>
       </div>
       );
   }
+}
+
+class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleSeasonChange = this.handleSeasonChange.bind(this);
+	}
+
+	handleSeasonChange(event) {
+		this.props.onSeasonInput(event.target.value);
+    console.log(event.target.value);
+    StatsActions.getSeasonLeaders(event.target.value);
+	}
+
+	render() {
+		return (
+      <div class="row">
+        <div class="col-md-12">
+  		    <form>
+    			  <fieldset class="fieldset">
+    			  	<legend class="season-legend">Choose a Season:</legend>
+    				    <label>
+    					    <select value={this.props.season} onChange={this.handleSeasonChange} class="form-control" id="season-select">
+      							<option value="2015-16">2015-16</option>
+      							<option value="2014-15">2014-15</option>
+      							<option value="2013-14">2013-14</option>
+      							<option value="2012-13">2012-13</option>
+      							<option value="2011-12">2011-12</option>
+      							<option value="2010-11">2010-11</option>
+      							<option value="2009-10">2009-10</option>
+      							<option value="2008-09">2008-09</option>
+      							<option value="2007-08">2007-08</option>
+      							<option value="2006-07">2006-07</option>
+      							<option value="2005-06">2005-06</option>
+      							<option value="2004-05">2004-05</option>
+      							<option value="2003-04">2003-04</option>
+      							<option value="2002-03">2002-03</option>
+      							<option value="2001-02">2001-02</option>
+      							<option value="2000-01">2000-01</option>
+      							<option value="1999-00">1999-00</option>
+      							<option value="1998-99">1998-99</option>
+      							<option value="1997-98">1997-98</option>
+      							<option value="1996-97">1996-97</option>
+    				    	</select>
+    				    </label>
+    			  </fieldset>
+  			  </form>
+        </div>
+      </div>
+		);
+	}
 }
